36da947dae1ec054a1d7b6e5d70f7a260a17c0fe
Radhika Rao <67799531+radhika757@users.noreply.github.com>
2024-10-29 14:58:30 +0530

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 
-function App() {
-  return (
+import './App.css';
 
-    <h1>Hello world</h1>
+import Title from 'antd/es/typography/Title';
+
+function App() {
+  
+  return (
+    <div className='container'>
+      <Title>Commit Message Generator</Title>
+      <span className='subtitle'>Create consistent and informative commit messages</span>
+      <div>
+       <select>
+        <option>Fix</option>
+        <option>Documentation</option>
+       </select>
+      </div>
+    </div>
   )
 }
 
